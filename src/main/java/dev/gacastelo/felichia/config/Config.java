061050c080da8ed254cf3d81de367f523b0c184e
gacastelo <gabriel.castelo5949@gmail.com>
2026-07-07 13:15:17 -0300

    chore: add .env for JWT_SECRET

--- src/main/java/dev/gacastelo/felichia/config/Config.java
+++ src/main/java/dev/gacastelo/felichia/config/Config.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
     public final static int daysToDeleteDisabledUsers = 3;
     public final static int daysToDeleteDisabledVaults = 4;
-    public final static String JWT_SECRET = "SECRET";
+    public final static String JWT_SECRET = System.getenv("JWT_SECRET");
 }
