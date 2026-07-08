73f7159a2b48e556a34b7793aa855d813ff74c0f
gacastelo <gabriel.castelo5949@gmail.com>
2026-07-07 22:26:45 -0300

    fix(user): UserController now receives UserCustomDetails by requests

--- src/main/java/dev/gacastelo/felichia/user/UserMapper.java
+++ src/main/java/dev/gacastelo/felichia/user/UserMapper.java
@@ -1,5 +1,6 @@
 package dev.gacastelo.felichia.user;
 
+import dev.gacastelo.felichia.auth.userCustomer.UserCustomDetails;
 import dev.gacastelo.felichia.user.dto.UserDTO;
 import dev.gacastelo.felichia.user.entity.User;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,16 @@ import org.springframework.stereotype.Component;
 public class UserMapper {
 
     public static UserDTO toDTO(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .name(user.getUsername())
+                .email(user.getEmail())
+                .emailVerified(user.isEmailVerified())
+                .enabled(user.isEnabled())
+                .build();
+    }
+
+    public static UserDTO toDTO(UserCustomDetails user) {
         return UserDTO.builder()
                 .id(user.getId())
                 .name(user.getUsername())
--- src/main/java/dev/gacastelo/felichia/user/controller/UserController.java
+++ src/main/java/dev/gacastelo/felichia/user/controller/UserController.java
@@ -1,9 +1,9 @@
 package dev.gacastelo.felichia.user.controller;
 
+import dev.gacastelo.felichia.auth.userCustomer.UserCustomDetails;
 import dev.gacastelo.felichia.user.UserMapper;
 import dev.gacastelo.felichia.user.dto.UpdateUserRequest;
 import dev.gacastelo.felichia.user.dto.UserDTO;
-import dev.gacastelo.felichia.user.entity.User;
 import dev.gacastelo.felichia.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,17 +20,17 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<UserDTO> getUser(@AuthenticationPrincipal User user) {
+    public ResponseEntity<UserDTO> getUser(@AuthenticationPrincipal UserCustomDetails user) {
         return ResponseEntity.ok(UserMapper.toDTO(user));
     }
 
     @PatchMapping
-    public UserDTO updateUser(@AuthenticationPrincipal User user, @RequestBody UpdateUserRequest updateUserRequest) {
+    public UserDTO updateUser(@AuthenticationPrincipal UserCustomDetails user, @RequestBody UpdateUserRequest updateUserRequest) {
         return userService.update(user.getId(), updateUserRequest);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal User user) {
+    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal UserCustomDetails user) {
         userService.delete(user.getId());
         return ResponseEntity.ok().build();
     }
